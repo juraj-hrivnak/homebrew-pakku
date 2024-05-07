a12a5c2646d1a2ab151084d4f35022878261e9cf
Juraj Hrivnák <bass.jurajhrivnak@gmail.com>
2024-05-07 17:19:20 +0200

    Create pakku.rb

---
+++ Formula/pakku.rb
@@ -0,0 +1,17 @@
+class Pakku < Formula
+  desc "Multiplatform modpack manager for Minecraft: Java Edition"
+  homepage "https://juraj-hrivnak.github.io/Pakku/"
+  url "https://github.com/juraj-hrivnak/Pakku/releases/download/v0.8.6/Pakku-0.8.6.tar"
+  hash "1c062722823af3e9e9ab8af4c6104a5b038f267eb2795f1ab8d4cdbd3cf62f83"
+  license "EUPL-1.2"
+
+  def install
+    rm_f Dir["bin/*.bat"]
+    libexec.install %w[bin lib]
+    (bin/"pakku").write_env_script libexec/"bin/Pakku", Language::Java.overridable_java_home_env
+  end
+
+  test do
+    assert_match "Could not read 'pakku-lock.json'", shell_output(bin/"pakku add jei", 1)
+  end
+end
