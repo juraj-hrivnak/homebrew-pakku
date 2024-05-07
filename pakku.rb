aa128d86db7c629b66db12670fce3d1486fed1fc
teksturepako <71150936+juraj-hrivnak@users.noreply.github.com>
2024-05-07 17:44:26 +0200

    Create pakku.rb

---
+++ pakku.rb
@@ -0,0 +1,17 @@
+class Pakku < Formula
+  desc "Multiplatform modpack manager for Minecraft: Java Edition"
+  homepage "https://juraj-hrivnak.github.io/Pakku/"
+  url "https://github.com/juraj-hrivnak/Pakku/releases/download/v0.8.7/Pakku-0.8.7.tar"
+  hash "0dd7bfc7b48a319aad3d8ece3eb9e046cb0aefb00b1ecb10cc6800357ff30b20"
+  license "EUPL-1.2"
+
+  def install
+    rm_f Dir["bin/*.bat"]
+    libexec.install %w[bin lib]
+    (bin/"pakku").write_env_script libexec/"bin/Pakku", Language::Java.overridable_java_home_env
+  end
+
+  test do
+    assert_match "Could not read 'pakku-lock.json'", shell_output(bin/"pakku add jei", 1)
+  end
+end
